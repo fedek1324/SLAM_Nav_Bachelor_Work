382792699daad1709b3e1306beea4b6bf940c4a2
fedek1324 <fkobyakov2@yandex.ru>
2023-05-20 22:35:46 +0300

    first commit

---
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Dropdown navigationTargetDropDown;
+    [SerializeField]
+    private List<Target> navigationTargetObjects = new List<Target>();
+    [SerializeField]
+    private Slider navigationYOffset;
+
+    //[SerializeField]
+    //private Camera topDownCamera;
+
+    //[SerializeField]
+    //private GameObject navTargetObject;
+
+    private NavMeshPath path; // current calculated papth
+    private LineRenderer line; // linerenderer to display path
+    private Vector3 targetPosition = Vector3.zero; // current target position
+
+    private bool lineToggle = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+        line.enabled = lineToggle;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (lineToggle && targetPosition != Vector3.zero)
+        {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            Vector3[] calculatedPathAndOffset = AddLineOffset();
+            line.SetPositions(calculatedPathAndOffset);
+        }
+    }
+
+    private Vector3[] AddLineOffset()
+    {
+        //if (navigationYOffset.value == 0)
+        //{
+        //    return path.corners;
+        //}
+
+        Vector3[] calculatedLine = new Vector3[path.corners.Length];
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            float yOffset = navigationYOffset.value;
+            calculatedLine[i] = path.corners[i] + new Vector3(0, yOffset, 0);
+        }
+        return calculatedLine;
+    }
+
+    public void SetCurrentNavigationTarget(int selectedValue)
+    {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if (currentTarget != null)
+        {
+            if (!line.enabled)
+            {
+                ToggleVisibility();
+            }
+
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
+    }
+}
---
+++ Assets/Scripts/SetUiText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SetUiText : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text textField;
+        [SerializeField]
+        private string fixedText;
+
+        public void OnSliderValueChanged(float numericValue)
+        {
+            textField.text = $"{fixedText}: {numericValue}";
+        }
+    }
+}
