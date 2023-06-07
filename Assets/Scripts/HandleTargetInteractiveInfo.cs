b029c70ba0bff8b2d26740c81f4ca375181aa53a
fedek1324 <fkobyakov2@yandex.ru>
2023-06-07 16:32:08 +0300

    added read csv func

--- Assets/Scripts/HandleTargetInteractiveInfo.cs
+++ Assets/Scripts/HandleTargetInteractiveInfo.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Data;
+using System.IO;
+using Packages;
+using UnityEngine.UI;
+using System.Linq;
 
 public class HandleTargetInteractiveInfo : MonoBehaviour
 {
+    [SerializeField]
+    Text debugText;
     // Start is called before the first frame update
     void Start()
     {
         AddInteractiveTextRecursive(gameObject);
+        ReadCSVFile(Path.Combine(Application.dataPath, "4floorInteractiveData.csv"));
     }
 
     public void AddInteractiveTextRecursive(GameObject navGO)
@@ -30,6 +38,34 @@ public class HandleTargetInteractiveInfo : MonoBehaviour
         TextMesh text = gameObject.GetComponentInChildren<TextMesh>();
         text.text = "AAAAAAAAAAAAAAAAA";
     }
+
+    public void ReadCSVFile(string filePath)
+    {
+        List<string[]> data = new List<string[]>();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] row = line.Split(',');
+
+                data.Add(row);
+            }
+        }
+
+        // Process the data as desired
+        foreach (string[] row in data)
+        {
+            //debugText.text = string.Join(',', row);
+            foreach (string cell in row)
+            {
+                debugText.text += cell + ", ";
+            }
+            debugText.text += "\n";
+        }
+    }
+
 
     public bool IsTarget(GameObject gameObject)
     {
